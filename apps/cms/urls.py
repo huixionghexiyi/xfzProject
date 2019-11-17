94e4ae7507349aa4d6f5a92bfe722d5e3d8c6c21
huixiong <huixionghexiyi@gmail.com>
2019-11-18 00:08:10 +0800

    使用AdmiLTE创建后台管理系统(初始化)

--- apps/cms/urls.py
+++ apps/cms/urls.py
@@ -5,5 +5,6 @@ app_name = 'cms'
 
 urlpatterns = [
     path("login/", views.login_view, name='login_view'),
-    path("index/", views.index, name='index')
+    path("index/", views.index, name='index'),
+    path("write_news/", views.WriteNewsView.as_view(), name='write_news'),
 ]
--- apps/cms/views.py
+++ apps/cms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic import View
 
 # Create your views here.
 def login_view(request):
@@ -12,3 +13,7 @@ def login_view(request):
 @staff_member_required(login_url='news:index')
 def index(request):
     return render(request,'cms/index.html')
+
+class WriteNewsView(View):
+    def get(self, request):
+        return render(request,'cms/write_news.html')
